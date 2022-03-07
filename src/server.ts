234005dead81d1d4a93f166017ea3f06d8a8baab
atirson <fabiano.oliveira2213@gmail.com>
2022-03-07 19:39:39 -0300

    config: Added config to debug.

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+app.post('/courses', (req, res) => {
+  const { name } = req.body;
+
+  return res.json({ name });
+});
+
 app.listen(3333, () => console.log('Server started on port 3333'));
