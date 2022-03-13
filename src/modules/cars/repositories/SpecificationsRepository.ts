3c8f7045c4235c8ac120d6aa2a697c96bf34d284
atirson <fabiano.oliveira2213@gmail.com>
2022-03-12 23:03:39 -0300

    feat: Finished implementation of method create specification.

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,35 @@
+import { Specification } from '../model/Specification';
+import {
+  ISpecificationsRepository,
+  ICreateSpecificationsDTO,
+} from './ISpecificationsRepository';
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  public create({ description, name }: ICreateSpecificationsDTO): void {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      description,
+      name,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specification);
+  }
+
+  findByName(name: string): Specification {
+    const specification = this.specifications.find(
+      specification => specification.name === name,
+    );
+
+    return specification;
+  }
+}
+
+export { SpecificationsRepository };
